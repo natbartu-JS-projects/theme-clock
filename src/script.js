3c8575ed12bf7b42492271b8843995fcb675371f
natacha <nat_bartu@hotmail.com>
2022-08-18 12:20:40 +1000

    add function for map a range of numbers to another range of numbers, and use the const elements to get the time by passing into a transform element, rotate function passing the scale function that handles the map function

--- src/script.js
+++ src/script.js
@@ -47,5 +47,39 @@ function setTime() {
   const day = time.getDay();
   const hours = time.getHours();
   const hoursForClock = hours % 12;
+  const minutes = time.getMinutes();
+  const seconds = time.getSeconds();
+
+  hourElement.style.transform = `translate(-50%, -100%) rotate(${scale(
+    hoursForClock,
+    0,
+    11,
+    0,
+    360
+  )}deg)`;
+
+  minuteElement.style.transform = `translate(-50%, -100%) rotate(${scale(
+    minutes,
+    0,
+    59,
+    0,
+    360
+  )}deg)`;
+
+  secondElement.style.transform = `translate(-50%, -100%) rotate(${scale(
+    seconds,
+    0,
+    59,
+    0,
+    360
+  )}deg)`;
 }
+
+// StackOverflow https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+const scale = (num, in_min, in_max, out_min, out_max) => {
+  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+};
+
 setTime();
+
+setInterval(setTime, 1000);
