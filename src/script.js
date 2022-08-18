fb097a568507684011bbd884cc93b381603d4691
natacha <nat_bartu@hotmail.com>
2022-08-18 12:31:11 +1000

    fix the hoursForClock function

--- src/script.js
+++ src/script.js
@@ -46,14 +46,15 @@ function setTime() {
   const month = time.getMonth();
   const day = time.getDay();
   const hours = time.getHours();
-  const hoursForClock = hours % 12;
+  const hoursForClock = hours >= 13 ? hours % 12 : hours;
   const minutes = time.getMinutes();
   const seconds = time.getSeconds();
+  const ampm = hours >= 12 ? "PM" : "AM";
 
   hourElement.style.transform = `translate(-50%, -100%) rotate(${scale(
     hoursForClock,
     0,
-    11,
+    12,
     0,
     360
   )}deg)`;
@@ -61,7 +62,7 @@ function setTime() {
   minuteElement.style.transform = `translate(-50%, -100%) rotate(${scale(
     minutes,
     0,
-    59,
+    60,
     0,
     360
   )}deg)`;
@@ -69,10 +70,14 @@ function setTime() {
   secondElement.style.transform = `translate(-50%, -100%) rotate(${scale(
     seconds,
     0,
-    59,
+    60,
     0,
     360
   )}deg)`;
+
+  timeElement.innerHTML = `${hoursForClock}:${
+    minutes < 10 ? `0${minutes}` : minutes
+  } ${ampm}`;
 }
 
 // StackOverflow https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
