aec3f00553aad7e77ca713f397c2086eb032bcf4
natacha <nat_bartu@hotmail.com>
2022-08-18 11:47:51 +1000

    get all the elements into query selector property

---
+++ src/script.js
@@ -0,0 +1,6 @@
+const hourElement = document.querySelector(".hour");
+const minuteElement = document.querySelector(".minute");
+const secondElement = document.querySelector(".second");
+const timeElement = document.querySelector(".time");
+const dateElement = document.querySelector(".date");
+const toggle = document.querySelector(".toggle");
